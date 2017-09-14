36502960c88cfd38cc1670d5a4ee821a5b41ffbc
hnorbert90 <hnorbert90@gmail.com>
2017-09-14 21:09:09 +0200

    initial commit

---
+++ src/Examples/Main.java
@@ -0,0 +1,20 @@
+
+package Examples;
+
+import static Examples.DublicatedCharacters.containsDublicatedChar;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        //1.1
+        System.out.println("1.1;");
+        System.out.println("tesTword has duplicated characters? : "+containsDublicatedChar("tesTword"));
+        System.out.println("testword has duplicated characters? : "+containsDublicatedChar("testword"));
+        //1.2
+        System.out.println("1.2;");
+        
+    }
+    
+}
