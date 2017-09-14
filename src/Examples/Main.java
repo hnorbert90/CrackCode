44cefaec7461caaa97a18a47c536ba6c1f09a588
hnorbert90 <hnorbert90@gmail.com>
2017-09-14 21:11:10 +0200

    initial commit

--- src/Examples/Main.java
+++ src/Examples/Main.java
@@ -8,7 +8,8 @@ public class Main {
 
     public static void main(String[] args) {
         
-        //1.1
+        //1.1 Implement an algorithm to determine if a string has all unique characters.
+        //What if you can not use additional data structures?
         System.out.println("1.1;");
         System.out.println("tesTword has duplicated characters? : "+containsDublicatedChar("tesTword"));
         System.out.println("testword has duplicated characters? : "+containsDublicatedChar("testword"));
